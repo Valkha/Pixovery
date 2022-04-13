77b58e10267551d2904649ff5807a6ea35abffc5
Valkha <adrien.garcon@gmail.com>
2022-04-13 10:52:38 +0200

    correction partie infos responsive

--- JS/upload_image.js
+++ JS/upload_image.js
@@ -1,6 +1,21 @@
 const image_input = document.querySelector("#image_input");
 let uploaded_image = "";
+let uploaded_image_path = "";
+
 
 image_input.addEventListener("change", function() {
-    const =
+    const image =
 })
+
+const image = `
+<a class="image" href="${uploaded_image_path}" data-lg-size="1920-1200">
+                        <div class="zoom">
+                            <picture>
+                                <source srcset="assets/images/PORTFOLIO/2-beseen.webp" media="(min-width: 800px)">
+                                <img src="assets/images/PORTFOLIO/2-beseen.webp" alt="">
+                            </picture>
+                        </div>
+                    </a>
+`
+
+document.querySelector('.gallery').insertAdjacentHTML('beforeend', image);
